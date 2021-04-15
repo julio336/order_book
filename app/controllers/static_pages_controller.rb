18494b3e7c6070213fe23b43b5b163288a4b57c9
julio336 <julio336@hotmail.com>
2021-04-15 15:57:13 -0500

    xrp

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -82,13 +82,13 @@ class StaticPagesController < ApplicationController
   	resp = Net::HTTP.get_response(URI.parse(url))
     data = JSON.parse(resp.body)
     data['bids'].each do |bid|
-    	if bid[1].to_f > 500
+    	if bid[1].to_f > 100000
 			@bid.store(bid[0].to_f.round(2), bid[1].to_f.round(2))
     	end
     end
 
     data['asks'].each do |ask|
-	   	if ask[1].to_f > 500
+	   	if ask[1].to_f > 100000
 			@ask.store(ask[0].to_f.round(2), ask[1].to_f.round(2))
 	    end
 	end 
@@ -96,7 +96,7 @@ class StaticPagesController < ApplicationController
 	url_price = "https://api.binance.com/api/v3/avgPrice?symbol=XRPUSDT"
 	resp = Net::HTTP.get_response(URI.parse(url_price))
     @price = JSON.parse(resp.body)
-    @price = @price['price'].to_f.round(2)
+    @price = @price['price'].to_f.round(3)
 	#puts data['bids'][0][1] #-> amount of bid
   end
 
