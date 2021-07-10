b226c9b8e45144e4f4d9f4e720bc3c996b255db1
julio336 <julio336@hotmail.com>
2021-07-10 15:06:13 -0500

    mana

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -125,4 +125,29 @@ class StaticPagesController < ApplicationController
   #puts data['bids'][0][1] #-> amount of bid
   end
 
+  def mana
+    @bid = Hash.new
+      @ask = Hash.new
+      url = "https://api.binance.com/api/v3/depth?symbol=MANAUSDT&limit=5000"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      data = JSON.parse(resp.body)
+      data['bids'].each do |bid|
+        if bid[1].to_f > 100000
+        @bid.store(bid[0].to_f.round(2), bid[1].to_f.round(2))
+        end
+      end
+
+      data['asks'].each do |ask|
+        if ask[1].to_f > 100000
+        @ask.store(ask[0].to_f.round(2), ask[1].to_f.round(2))
+        end
+    end 
+
+    url_price = "https://api.binance.com/api/v3/avgPrice?symbol=MANAUSDT"
+    resp = Net::HTTP.get_response(URI.parse(url_price))
+      @price = JSON.parse(resp.body)
+      @price = @price['price'].to_f.round(3)
+    #puts data['bids'][0][1] #-> amount of bid
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'ltc', controller: 'static_pages'
   get 'xrp', controller: 'static_pages'
   get 'ada', controller: 'static_pages'
+  get 'mana', controller: 'static_pages'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
